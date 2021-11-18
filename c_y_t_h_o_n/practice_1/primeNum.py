e14640e1d33b880808650d711022eb5830134ac6
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2021-11-18 14:36:27 +0600

    Added Cython dir
    This dir contains all cython code examples

---
+++ c_y_t_h_o_n/practice_1/primeNum.py
@@ -0,0 +1,15 @@
+def prime_finder_vanilla(amount):
+    primes = []
+
+    found = 0
+    number = 2
+    while found < amount:
+        for x in primes:
+            if number % x == 0:
+                break
+        else:
+            primes.append(number)
+            found += 1
+        number += 1
+
+    return primes
